f50aba7d00691b9a70867f66dab97b5312ef5620
Jose Flores <jose.flores94h@gmail.com>
2025-06-21 10:46:24 -0600

    Configuracion inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'ng-task-18-d9afa',
+        appId: '1:422505285233:web:52ca5f652802679bf0bdd0',
+        storageBucket: 'ng-task-18-d9afa.firebasestorage.app',
+        apiKey: 'AIzaSyBfAvqcvHO_aFqNSNEz_yWtSZsKvZDoW5c',
+        authDomain: 'ng-task-18-d9afa.firebaseapp.com',
+        messagingSenderId: '422505285233',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
