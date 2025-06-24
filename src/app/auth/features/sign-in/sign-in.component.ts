4b241d82ea44bacfdf3808ed4fc5a3217cdee0a6
Jose Flores <jose.flores94h@gmail.com>
2025-06-23 20:49:07 -0600

    Sign Up with routes

---
+++ src/app/auth/features/sign-in/sign-in.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [],
+  templateUrl: './sign-in.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class SignInComponent {}
---
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -0,0 +1,66 @@
+import { hasEmailError, isRequired } from './../../utils/validators';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../data-access/auth.service';
+import { toast } from 'ngx-sonner';
+import { Router } from '@angular/router';
+
+interface FormSignUp {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
+@Component({
+  selector: 'app-sign-up',
+  imports: [ReactiveFormsModule],
+  templateUrl: './sign-up.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class SignUpComponent {
+  private _formBuilder = inject(FormBuilder);
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
+
+  form = this._formBuilder.group<FormSignUp>({
+    email: this._formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: this._formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  async submit() {
+    if (this.form.invalid) return;
+
+    try {
+      const { email, password } = this.form.value;
+
+      if (!email || !password) return;
+
+      await this._authService.signUp({ email, password });
+
+      toast.success('Usuario creado correctamente!');
+      this.form.reset();
+      this._router.navigateByUrl('/tasks');
+    } catch (error) {
+      toast.error('Error al crear el usuario. Por favor, inténtalo de nuevo.');
+      console.error('Error al crear el usuario:', error);
+      this.form.reset();
+    }
+  }
+
+  isRequired(field: 'email' | 'password') {
+    return isRequired(field, this.form);
+  }
+
+  hasEmailError() {
+    return hasEmailError(this.form);
+  }
+}
